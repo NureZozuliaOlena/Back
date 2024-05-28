a26e85763dcc34cabef8d5e340a6d93ea29496d5
SkliarovaAnastasiia <skliarova16@gmail.com>
2024-05-28 11:13:38 +0300

    added interface and serivice for auth

--- Back/Interfaces/IAuthService.cs
+++ Back/Interfaces/IAuthService.cs
@@ -1,6 +1,11 @@
-﻿namespace Back.Interfaces
+﻿using Back.Models;
+using Back.ViewModels;
+using System.Threading.Tasks;
+
+namespace Back.Interfaces
 {
-    public class IAuthService
+    public interface IAuthService
     {
+
     }
 }
